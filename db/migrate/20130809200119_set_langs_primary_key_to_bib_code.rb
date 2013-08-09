8b116c7297ccde45e0920ddaee26ffa4cc7a54b5
Doug Emery <emeryr@upenn.edu>
2013-08-09 16:42:04 -0400

    Make id column nullable

--- db/migrate/20130809200119_set_langs_primary_key_to_bib_code.rb
+++ db/migrate/20130809200119_set_langs_primary_key_to_bib_code.rb
@@ -1,6 +1,7 @@
 class SetLangsPrimaryKeyToBibCode < ActiveRecord::Migration
   def up
     execute "ALTER TABLE iso639_config_langs DROP CONSTRAINT iso639_config_langs_pkey;"
+    change_column :iso639_config_langs, :id, null: true
     execute "ALTER TABLE iso639_config_langs ADD PRIMARY KEY (bib_code);"
   end
 
