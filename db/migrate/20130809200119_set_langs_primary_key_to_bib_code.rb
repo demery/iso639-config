a2ef649e765320f1cec5a4f142988118187509d3
Doug Emery <emeryr@upenn.edu>
2013-08-09 16:09:27 -0400

    Have to drop pkey before add a new one

--- db/migrate/20130809200119_set_langs_primary_key_to_bib_code.rb
+++ db/migrate/20130809200119_set_langs_primary_key_to_bib_code.rb
@@ -1,9 +1,11 @@
 class SetLangsPrimaryKeyToBibCode < ActiveRecord::Migration
   def up
+    execute "ALTER TABLE iso639_config_langs DROP CONSTRAINT iso639_config_langs_pkey;"
     execute "ALTER TABLE iso639_config_langs ADD PRIMARY KEY (bib_code);"
   end
 
   def down
+    execute "ALTER TABLE iso639_config_langs DROP CONSTRAINT iso639_config_langs_pkey;"
     execute "ALTER TABLE iso639_config_langs ADD PRIMARY KEY (id);"
   end
 end
